bff540c249113ce3283a7092e5923fb29e90c275
RamosJoey <18160@my.sanctamaria.school.nz>
2022-02-17 09:24:18 +1300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Welcome to my site</h1>"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
