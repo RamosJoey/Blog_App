9f5eea2d8d7cf119b2ac4ada55d16f61addd4ac1
RamosJoey <18160@my.sanctamaria.school.nz>
2022-03-07 23:34:35 +1300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, url_for, flash, redirect
+
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'bb55018cb9a7d19d63ad564aa22cc383'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
 posts = [
          
